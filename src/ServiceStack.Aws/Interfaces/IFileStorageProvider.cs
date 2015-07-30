94ad1aec58206174d7f87aa0e05517ff74f62524
Chad Boyd <chad.boyd@gmail.com>
2015-07-30 10:42:46 -0700

    temp commit to move to desktop

--- src/ServiceStack.Aws/Interfaces/IFileStorageProvider.cs
+++ src/ServiceStack.Aws/Interfaces/IFileStorageProvider.cs
@@ -1,22 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using ServiceStack.Aws.S3;
 
 namespace ServiceStack.Aws.Interfaces
 {
     public interface IFileStorageProvider
     {
-        //void Download(FileMetaData thisProviderMeta, FileMetaData downloadToMeta);
-        //void CopyTo(FileMetaData thisProviderMeta, IFileStorageProvider targetProvider, FileMetaData targetMeta);
-        //void InjectTransportInfo(TransportInfo transportInfo);
-        //Byte[] Get(FileMetaData fileData);
-        //void Store(FileMetaData fileData);
-        //void Store(FileMetaData sourceMeta, FileMetaData targetMeta);
-        //void Delete(FileMetaData fileData);
-        //void DeleteFolder(string folderName, Boolean recursive);
-        //void CreateFolder(string folderName);
-        //void Move(string source, string target);
-        //Boolean Exists(FileMetaData fileData);
+        void Download(string thisFileName, string localFileSystemTargetFileName);
+        void Download(FileSystemObject thisFso, FileSystemObject localFileSystemFso);
 
-        //IEnumerable<string> ListFolder(string folderName, Boolean recursive = false);
+        Byte[] Get(string thisFileName);
+        Byte[] Get(FileSystemObject fso);
+
+        void Store(Byte[] bytes, string targetFileName);
+        void Store(Byte[] bytes, FileSystemObject fso);
+        void Store(string localFileSystemSourceFileName, string targetFileName);
+        void Store(FileSystemObject localFileSystemFso, FileSystemObject targetFso);
+
+        void Delete(string fileName);
+        void Delete(FileSystemObject fso);
+        void Delete(IEnumerable<string> fileNames);
+        void Delete(IEnumerable<FileSystemObject> fsos);
+
+        void Copy(string thisFileName, string copyToFileName, IFileStorageProvider targetProvider = null);
+        void Copy(FileSystemObject thisFso, FileSystemObject targetFso, IFileStorageProvider targetProvider = null);
+
+        void Move(string thisFileName, string moveToFileName, IFileStorageProvider targetProvider = null);
+        void Move(FileSystemObject sourceFso, FileSystemObject targetFso, IFileStorageProvider targetProvider = null);
+
+        bool Exists(string fileName);
+        bool Exists(FileSystemObject fso);
+
+        IEnumerable<string> ListFolder(string folderName, Boolean recursive = false);
+        void DeleteFolder(string folderName, Boolean recursive);
+        void CreateFolder(string folderName);
     }
 }
---
+++ src/ServiceStack.Aws/S3/BaseFileStorageProvider.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using ServiceStack.Aws.Interfaces;
+
+namespace ServiceStack.Aws.S3
+{
+    public abstract class BaseFileStorageProvider : IFileStorageProvider
+    {
+        protected BaseFileStorageProvider() { }
+
+        public void Download(string thisFileName, string localFileSystemTargetFileName)
+        {
+            var sourceFso = new FileSystemObject(thisFileName);
+            var targetFso = new FileSystemObject(localFileSystemTargetFileName);
+            Download(sourceFso, targetFso);
+        }
+
+        public Byte[] Get(string thisFileName)
+        {
+            var fso = new FileSystemObject(thisFileName);
+            return Get(fso);
+        }
+
+        public void Store(Byte[] bytes, string targetFileName)
+        {
+            var fso = new FileSystemObject(targetFileName);
+            Store(bytes, fso);
+        }
+
+        public void Store(string localFileSystemSourceFileName, string targetFileName)
+        {
+            var localFileSystemFso = new FileSystemObject(localFileSystemSourceFileName);
+            var fso = new FileSystemObject(targetFileName);
+            Store(localFileSystemFso, fso);
+        }
+
+        public void Delete(string fileName)
+        {
+            var fso = new FileSystemObject(fileName);
+            Delete(fso);
+        }
+
+        public void Delete(IEnumerable<string> fileNames)
+        {
+            foreach (var fileName in fileNames)
+            {
+                Delete(fileName);
+            }
+        }
+
+        public void Copy(string thisFileName, string copyToFileName, IFileStorageProvider targetProvider = null)
+        {
+            var sourceFso = new FileSystemObject(thisFileName);
+            var targetFso = new FileSystemObject(copyToFileName);
+            Copy(sourceFso, targetFso, targetProvider);
+        }
+
+        public void Move(string thisFileName, string moveToFileName, IFileStorageProvider targetProvider = null)
+        {
+            var sourceFso = new FileSystemObject(thisFileName);
+            var targetFso = new FileSystemObject(moveToFileName);
+            Move(sourceFso, targetFso, targetProvider);
+        }
+
+        public bool Exists(string fileName)
+        {
+            var fso = new FileSystemObject(fileName);
+            return Exists(fso);
+        }
+
+        public abstract void Download(FileSystemObject thisFso, FileSystemObject localFileSystemFso);
+        public abstract byte[] Get(FileSystemObject fso);
+        public abstract void Store(Byte[] bytes, FileSystemObject fso);
+        public abstract void Store(FileSystemObject localFileSystemFso, FileSystemObject targetFso);
+        public abstract void Delete(FileSystemObject fso);
+        public abstract void Delete(IEnumerable<FileSystemObject> fsos);
+        public abstract void Copy(FileSystemObject sourceFso, FileSystemObject targetFso, IFileStorageProvider targetProvider = null);
+        public abstract void Move(FileSystemObject sourceFso, FileSystemObject targetFso, IFileStorageProvider targetProvider = null);
+        public abstract bool Exists(FileSystemObject fso);
+        public abstract IEnumerable<string> ListFolder(string folderName, bool recursive = false);
+        public abstract void DeleteFolder(string folderName, bool recursive);
+        public abstract void CreateFolder(string folderName);
+    }
+}
