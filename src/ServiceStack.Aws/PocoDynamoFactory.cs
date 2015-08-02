39a71d1f803e1cebd726b1db6ab633e95e40ddd0
Demis Bellot <demis.bellot@gmail.com>
2015-08-03 02:42:58 +0800

    Create IPocoDynamoFactory if factory is preferred

---
+++ src/ServiceStack.Aws/PocoDynamoFactory.cs
@@ -0,0 +1,26 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amib.Threading;
+
+namespace ServiceStack.Aws
+{
+    public interface IPocoDynamoFactory
+    {
+        IPocoDynamo GetClient();
+    }
+
+    public class PocoDynamoFactory : IPocoDynamoFactory
+    {
+        private readonly Func<IAmazonDynamoDB> factory;
+
+        public PocoDynamoFactory(Func<IAmazonDynamoDB> factory)
+        {
+            this.factory = factory;
+        }
+
+        public IPocoDynamo GetClient()
+        {
+            return new PocoDynamo(factory());
+        }
+    }
+}
