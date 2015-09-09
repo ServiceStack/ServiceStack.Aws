d16faf05efb56457cecbcd59a60af03ed1fae7ce
Demis Bellot <demis.bellot@gmail.com>
2015-09-09 13:01:10 -0700

    add AWS build proj

---
+++ build/build.bat
@@ -0,0 +1,3 @@
+SET MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+
+%MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=0
