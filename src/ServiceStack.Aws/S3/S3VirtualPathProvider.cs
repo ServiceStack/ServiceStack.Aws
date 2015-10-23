e2fe8ee270c2ac08c348e06030c8aeaace003eb8
Demis Bellot <demis.bellot@gmail.com>
2015-10-22 22:05:44 -0700

    Add initial S3 VirtualPathProvider tests

--- src/ServiceStack.Aws/S3/S3VirtualPathProvider.cs
+++ src/ServiceStack.Aws/S3/S3VirtualPathProvider.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using Amazon.S3;
+using Amazon.S3.Model;
 using ServiceStack.IO;
 using ServiceStack.Text;
 using ServiceStack.VirtualPath;
@@ -10,9 +13,14 @@ namespace ServiceStack.Aws.S3
 {
     public class S3VirtualPathProvider : AbstractVirtualPathProviderBase, IWriteableVirtualPathProvider
     {
-        public S3VirtualPathProvider(IAppHost appHost)
+        public IAmazonS3 Client { get; private set; }
+        public string BucketName { get; private set; }
+
+        public S3VirtualPathProvider(IAmazonS3 client, string bucketName, IAppHost appHost)
             : base(appHost)
         {
+            this.Client = client;
+            this.BucketName = bucketName;
             this.rootDirectory = new S3VirtualDirectory(this);
         }
 
@@ -42,6 +50,11 @@ namespace ServiceStack.Aws.S3
             rootDirectory.AddFile(filePath, contents);
         }
 
+        public void AddFile(string filePath, Stream stream)
+        {
+            rootDirectory.AddFile(filePath, stream);
+        }
+
         public override IVirtualFile GetFile(string virtualPath)
         {
             return rootDirectory.GetFile(virtualPath)
@@ -51,36 +64,49 @@ namespace ServiceStack.Aws.S3
 
     public class S3VirtualDirectory : AbstractVirtualDirectoryBase
     {
-        public S3VirtualDirectory(IVirtualPathProvider owningProvider)
-            : base(owningProvider)
+        private S3VirtualPathProvider pathProvider;
+
+        public S3VirtualDirectory(S3VirtualPathProvider pathProvider)
+            : base(pathProvider)
         {
+            this.pathProvider = pathProvider;
             this.files = new List<S3VirtualFile>();
             this.dirs = new List<S3VirtualDirectory>();
             this.DirLastModified = DateTime.MinValue;
         }
 
         public S3VirtualDirectory(IVirtualPathProvider owningProvider, IVirtualDirectory parentDirectory)
-            : base(owningProvider, parentDirectory)
-        { }
+            : base(owningProvider, parentDirectory) { }
+
+        static readonly char DirSep = '/';
 
         public DateTime DirLastModified { get; set; }
+
         public override DateTime LastModified
         {
             get { return DirLastModified; }
         }
 
         public List<S3VirtualFile> files;
-
         public override IEnumerable<IVirtualFile> Files
         {
             get { return files; }
         }
 
         public List<S3VirtualDirectory> dirs;
-
         public override IEnumerable<IVirtualDirectory> Directories
         {
             get { return dirs; }
+        }
+
+        public IAmazonS3 Client
+        {
+            get { return pathProvider.Client; }
+        }
+
+        public string BucketName
+        {
+            get { return pathProvider.BucketName; }
         }
 
         public string DirName { get; set; }
@@ -91,8 +117,30 @@ namespace ServiceStack.Aws.S3
 
         public override IVirtualFile GetFile(string virtualPath)
         {
-            virtualPath = StripBeginningDirectorySeparator(virtualPath);
-            return files.FirstOrDefault(x => x.FilePath == virtualPath);
+            try
+            {
+                var response = Client.GetObject(new GetObjectRequest
+                {
+                    Key = StripDirSeparatorPrefix(virtualPath),
+                    BucketName = BucketName,
+                });
+
+                return new S3VirtualFile(pathProvider, this)
+                {
+                    FileName = response.Key.SplitOnLast('/').Last(),
+                    FilePath = response.Key,
+                    ContentType = response.Headers.ContentType,
+                    FileLastModified = response.LastModified,
+                    Stream = response.ResponseStream,
+                };
+            }
+            catch (AmazonS3Exception ex)
+            {
+                if (ex.StatusCode == HttpStatusCode.NotFound)
+                    return null;
+
+                throw;
+            }
         }
 
         public override IEnumerator<IVirtualNode> GetEnumerator()
@@ -128,27 +176,29 @@ namespace ServiceStack.Aws.S3
             return null;
         }
 
-        static readonly char[] DirSeps = new[] { '\\', '/' };
         public void AddFile(string filePath, string contents)
         {
-            filePath = StripBeginningDirectorySeparator(filePath);
-            this.files.Add(new S3VirtualFile(VirtualPathProvider, this)
-            {
-                FilePath = filePath,
-                FileName = filePath.Split(DirSeps).Last(),
-                TextContents = contents,
+            Client.PutObject(new PutObjectRequest {
+                Key = StripDirSeparatorPrefix(filePath),
+                BucketName = pathProvider.BucketName,
+                ContentBody = contents,
             });
         }
 
-        private static string StripBeginningDirectorySeparator(string filePath)
+        public void AddFile(string filePath, Stream stream)
         {
-            if (string.IsNullOrEmpty(filePath))
-                return filePath;
+            Client.PutObject(new PutObjectRequest {
+                Key = StripDirSeparatorPrefix(filePath),
+                BucketName = pathProvider.BucketName,
+                InputStream = stream,
+            });
+        }
 
-            if (DirSeps.Any(d => filePath[0] == d))
-                return filePath.Substring(1);
-
-            return filePath;
+        private static string StripDirSeparatorPrefix(string filePath)
+        {
+            return string.IsNullOrEmpty(filePath)
+                ? filePath
+                : (filePath[0] == DirSep ? filePath.Substring(1) : filePath);
         }
     }
 
@@ -163,6 +213,8 @@ namespace ServiceStack.Aws.S3
         public string FilePath { get; set; }
 
         public string FileName { get; set; }
+
+        public string ContentType { get; set; }
 
         public override string Name
         {
@@ -188,13 +240,23 @@ namespace ServiceStack.Aws.S3
             }
         }
 
-        public string TextContents { get; set; }
+        private string textContents;
+        public string TextContents
+        {
+            get { return textContents ?? (textContents = ReadAllText()); }
+        }
 
-        public byte[] ByteContents { get; set; }
+        private byte[] byteContents;
+        public byte[] ByteContents
+        {
+            get { return byteContents ?? (byteContents = OpenRead().ReadFully()); }
+        }
+
+        public Stream Stream { get; set; }
 
         public override Stream OpenRead()
         {
-            return MemoryStreamFactory.GetStream(ByteContents ?? (TextContents ?? "").ToUtf8Bytes());
+            return Stream;
         }
     }
 
---
+++ tests/ServiceStack.Aws.Tests/S3/VirtualPathProviderTests.cs
@@ -0,0 +1,63 @@
+﻿using Amazon.S3;
+using NUnit.Framework;
+using ServiceStack.Aws.S3;
+using ServiceStack.IO;
+using ServiceStack.Testing;
+using ServiceStack.VirtualPath;
+
+namespace ServiceStack.Aws.Tests.S3
+{
+    public class S3VirtualPathProviderTests : VirtualPathProviderTests
+    {
+        private IAmazonS3 client = AwsConfig.CreateAmazonS3Client();
+
+        public override IVirtualPathProvider GetPathProvider()
+        {
+            return new S3VirtualPathProvider(client, "ss-ci-test", appHost);
+        }
+    }
+
+    public class InMemoryVirtualPathProviderTests : VirtualPathProviderTests
+    {
+        public override IVirtualPathProvider GetPathProvider()
+        {
+            return new InMemoryVirtualPathProvider(appHost);
+        }
+    }
+
+    [TestFixture]
+    public abstract class VirtualPathProviderTests
+    {
+        public abstract IVirtualPathProvider GetPathProvider();
+
+        protected ServiceStackHost appHost;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            appHost = new BasicAppHost()
+                .Init();
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            appHost.Dispose();
+        }
+
+        [Test]
+        public void Can_create_file()
+        {
+            var pathProvider = GetPathProvider();
+
+            var filePath = "dir/file.txt";
+            pathProvider.AddFile(filePath, "file");
+
+            var file = pathProvider.GetFile(filePath);
+
+            Assert.That(file.ReadAllText(), Is.EqualTo("file"));
+            Assert.That(file.Name, Is.EqualTo(filePath));
+            Assert.That(file.Extension, Is.EqualTo("txt"));
+        }
+    }
+}
