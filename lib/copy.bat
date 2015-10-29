d805ac2987d2f55ec9c64685c92c0a657e921979
Demis Bellot <demis.bellot@gmail.com>
2015-10-28 21:37:32 -0700

    Update SS dll

--- lib/copy.bat
+++ lib/copy.bat
@@ -1,5 +1,6 @@
 COPY C:\src\ServiceStack\src\ServiceStack.Razor\bin\Release\* .
 COPY C:\src\ServiceStack\src\ServiceStack.Server\bin\Release\* .
+COPY C:\src\ServiceStack.Aws\src\ServiceStack.Aws\bin\Release\ServiceStack.Aws.* .
 
 REM SET BUILD=Debug
 SET BUILD=Release
