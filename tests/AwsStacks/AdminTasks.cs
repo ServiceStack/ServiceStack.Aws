c452de9b98fbd90526ab3e1e83570ad708dab0f0
Demis Bellot <demis.bellot@gmail.com>
2015-10-29 14:18:44 -0700

    Add new AwsStacks project with links to AWS TODO and RazorRockstars.S3

---
+++ tests/AwsStacks/AdminTasks.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using ServiceStack;
+using ServiceStack.Aws.S3;
+using ServiceStack.Testing;
+
+namespace AwsStacks
+{
+    [TestFixture]
+    public class AdminTasks
+    {
+        private readonly ServiceStackHost appHost;
+        S3VirtualPathProvider s3;
+
+        public AdminTasks()
+        {
+            appHost = new BasicAppHost().Init();
+            s3 = AwsConfig.CreateS3VirtualPathProvider(appHost, AwsConfig.S3BucketName);
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            appHost.Dispose();
+        }
+
+        [Test]
+        public void Drop_and_Create_ssawsdemo_Bucket()
+        {
+            try
+            {
+                s3.ClearBucket();
+                s3.AmazonS3.DeleteBucket(AwsConfig.S3BucketName);
+            }
+            catch { }
+
+            s3.AmazonS3.PutBucket(AwsConfig.S3BucketName);
+        }
+        
+    }
+}
---
+++ tests/AwsStacks/AppHost.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Amazon;
+using Amazon.S3;
+using Funq;
+using ServiceStack;
+using ServiceStack.Aws.DynamoDb;
+using ServiceStack.Aws.S3;
+using ServiceStack.IO;
+using ServiceStack.Razor;
+using ServiceStack.Text;
+using ServiceStack.VirtualPath;
+using Todos;
+
+namespace AwsStacks
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : base("AWS Examples", typeof(TodoService).Assembly) { }
+
+        public override void Configure(Container container)
+        {
+            JsConfig.EmitCamelCaseNames = true;
+
+            var s3Client = new AmazonS3Client(AwsConfig.AwsAccessKey, AwsConfig.AwsSecretKey, RegionEndpoint.USEast1);
+            container.Register<IWriteableVirtualPathProvider>(c =>
+                new S3VirtualPathProvider(s3Client, AwsConfig.S3BucketName, this));
+
+            container.Register<IPocoDynamo>(c => new PocoDynamo(AwsConfig.CreateAmazonDynamoDb()));
+            var db = container.Resolve<IPocoDynamo>();
+            db.RegisterTable<Todo>();
+            db.InitSchema();
+
+            Plugins.Add(new RazorFormat());
+        }
+
+        public override List<IVirtualPathProvider> GetVirtualPathProviders()
+        {
+            var pathProviders = base.GetVirtualPathProviders();
+            pathProviders.Add(Container.Resolve<IWriteableVirtualPathProvider>());
+            return pathProviders;
+        }
+    }
+}
