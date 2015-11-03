fb6b9b5c746236dbe7ff47243e9f94d7b56c4f7b
Demis Bellot <demis.bellot@gmail.com>
2015-11-03 12:52:02 -0800

    Add OAuth libs used by awsauth to copy list

--- lib/copy.bat
+++ lib/copy.bat
@@ -1,6 +1,9 @@
 COPY C:\src\ServiceStack\src\ServiceStack.Razor\bin\Release\* .
 COPY C:\src\ServiceStack\src\ServiceStack.Server\bin\Release\* .
-COPY C:\src\ServiceStack.Aws\src\ServiceStack.Aws\bin\Release\ServiceStack.Aws.* .
+COPY C:\src\ServiceStack\src\ServiceStack.Authentication.OAuth2\bin\Release\ServiceStack.Authentication.OAuth2.* .
+COPY C:\src\ServiceStack\src\ServiceStack.Authentication.OpenId\bin\Release\ServiceStack.Authentication.OpenId.* .
+REM COPY C:\src\ServiceStack.Aws\src\ServiceStack.Aws\bin\Release\ServiceStack.Aws.* .
+COPY C:\src\ServiceStack.Aws\src\ServiceStack.Aws\bin\Debug\ServiceStack.Aws.* .
 
 REM SET BUILD=Debug
 SET BUILD=Release
