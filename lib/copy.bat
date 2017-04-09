f1efcb562940e7f088faf18861c98199ca104ebd
Demis Bellot <demis.bellot@gmail.com>
2017-04-09 05:00:38 -0400

    clean up old proj json + restructure deps into platform folders

--- lib/copy.bat
+++ lib/copy.bat
@@ -1,13 +1,29 @@
-COPY C:\src\ServiceStack\src\ServiceStack.Razor\bin\Release\* .
-COPY C:\src\ServiceStack\src\ServiceStack.Server\bin\Release\* .
-COPY C:\src\ServiceStack\src\ServiceStack.Authentication.OAuth2\bin\Release\ServiceStack.Authentication.OAuth2.* .
-COPY C:\src\ServiceStack\src\ServiceStack.Authentication.OpenId\bin\Release\ServiceStack.Authentication.OpenId.* .
-COPY C:\src\ServiceStack.Aws\src\ServiceStack.Aws\bin\Release\ServiceStack.Aws.* .
-REM COPY C:\src\ServiceStack.Aws\src\ServiceStack.Aws\bin\Debug\ServiceStack.Aws.* .
-
 REM SET BUILD=Debug
 SET BUILD=Release
 
-COPY ..\src\ServiceStack.Aws\bin\%BUILD%\ServiceStack.Aws.* ..\..\ServiceStack\lib
+COPY "..\..\ServiceStack\src\ServiceStack.Interfaces\bin\%BUILD%\portable40-net45+sl5+win8+wp8+wpa81\ServiceStack.Interfaces.*" pcl
 
-COPY C:\src\ServiceStack\lib\signed\* signed\
+COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.* net45
+COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\netstandard1.1\ServiceStack.Text.* netstandard1.1
+COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\netstandard1.3\ServiceStack.Text.* netstandard1.3
+COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\Signed\net45\ServiceStack.Text.* signed
+
+COPY ..\..\ServiceStack\src\ServiceStack.Client\bin\%BUILD%\net45\ServiceStack.Client.* net45
+COPY ..\..\ServiceStack\src\ServiceStack.Client\bin\%BUILD%\netstandard1.1\ServiceStack.Client.* netstandard1.1
+COPY ..\..\ServiceStack\src\ServiceStack.Client\bin\%BUILD%\netstandard1.6\ServiceStack.Client.* netstandard1.6
+COPY ..\..\ServiceStack\src\ServiceStack.Client\bin\Signed\net45\ServiceStack.Client.* signed
+
+COPY ..\..\ServiceStack\src\ServiceStack.Common\bin\%BUILD%\net45\ServiceStack.Common.* net45
+COPY ..\..\ServiceStack\src\ServiceStack.Common\bin\%BUILD%\netstandard1.3\ServiceStack.Common.* netstandard1.3
+COPY ..\..\ServiceStack\src\ServiceStack.Common\bin\Signed\net45\ServiceStack.Common.* signed
+
+COPY ..\..\ServiceStack\src\ServiceStack\bin\%BUILD%\net45\ServiceStack.dll net45
+COPY ..\..\ServiceStack\src\ServiceStack\bin\%BUILD%\net45\ServiceStack.xml net45
+COPY ..\..\ServiceStack\src\ServiceStack\bin\%BUILD%\netstandard1.6\ServiceStack.dll netstandard1.6
+COPY ..\..\ServiceStack\src\ServiceStack\bin\%BUILD%\netstandard1.6\ServiceStack.xml netstandard1.6
+COPY ..\..\ServiceStack\src\ServiceStack\bin\Signed\net45\ServiceStack.dll signed
+COPY ..\..\ServiceStack\src\ServiceStack\bin\Signed\net45\ServiceStack.xml signed
+
+COPY ..\..\ServiceStack\src\ServiceStack.Server\bin\%BUILD%\net45\ServiceStack.Server.* net45
+COPY ..\..\ServiceStack\src\ServiceStack.Server\bin\%BUILD%\netstandard1.6\ServiceStack.Server.* netstandard1.6
+COPY ..\..\ServiceStack\src\ServiceStack.Server\bin\Signed\net45\ServiceStack.Server.* net45
