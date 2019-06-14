92386a96a501dc34e1a47b55edc0b3f8c3fdd18f
Demis Bellot <demis.bellot@gmail.com>
2019-06-13 20:18:06 -0400

    Update AwsScripts.cs

--- src/ServiceStack.Aws/AwsScripts.cs
+++ src/ServiceStack.Aws/AwsScripts.cs
@@ -19,10 +19,10 @@ namespace ServiceStack.Aws
         public AmazonS3Client amazonS3Client(string awsAccessKeyId, string awsSecretAccessKey, string region) =>
             new AmazonS3Client(awsAccessKeyId, awsSecretAccessKey, RegionEndpoint.GetBySystemName(region));
         
-        public S3VirtualFiles s3VirtualFiles(AmazonS3Client s3Client, string bucketName) => 
+        public S3VirtualFiles vfsS3(AmazonS3Client s3Client, string bucketName) => 
             new S3VirtualFiles(s3Client, bucketName);
 
-        public S3VirtualFiles s3VirtualFiles(string awsAccessKeyId, string awsSecretAccessKey, string region, string bucketName) => 
+        public S3VirtualFiles vfsS3(string awsAccessKeyId, string awsSecretAccessKey, string region, string bucketName) => 
             new S3VirtualFiles(amazonS3Client(awsAccessKeyId, awsSecretAccessKey, region), bucketName);
     }
     
